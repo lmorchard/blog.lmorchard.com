f301e5276b8476e3606d77d208aa7f27df089951
Les Orchard <me@lmorchard.com>
2014-10-17 17:23:24 -0400

    First steps toward S3 deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var awspublish = require('gulp-awspublish');
 
 var config = require('./config');
 var requireDir = require('require-dir');
@@ -18,4 +19,15 @@ gulp.task('watch', ['static:watch', 'posts:watch', 'pages:watch', 'archives:watc
 
 gulp.task('server', ['build', 'watch'], function () {
   connect.server({ port: 4001, root: 'build' });
+});
+
+gulp.task('deploy', function () {
+  var publisher = awspublish.create(config.aws);
+  var headers = {
+    // 'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+  return gulp.src('./build/**')
+    .pipe(publisher.publish(headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter());
 });
