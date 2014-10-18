140bb7ddd62d6c8012932780c2dc264294c09f2f
Les Orchard <me@lmorchard.com>
2014-10-17 22:32:33 -0400

    First stab at feeds

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,13 @@ try {
 var requireDir = require('require-dir');
 requireDir('./lib/tasks', { recurse: true });
 
-gulp.task('build', ['static', 'posts', 'pages', 'archives']);
+gulp.task('build', [
+  'static', 'posts', 'pages', 'archives', 'feeds'
+]);
 
-gulp.task('watch', ['static:watch', 'posts:watch', 'pages:watch', 'archives:watch']);
+gulp.task('watch', [
+  'static:watch', 'posts:watch', 'pages:watch', 'archives:watch'
+]);
 
 gulp.task('server', ['build', 'watch'], function () {
   connect.server({ port: 4001, root: 'build' });
