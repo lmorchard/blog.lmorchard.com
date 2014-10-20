455bd06f60cbae0a1312e154077bdbf2407fa89e
Les Orchard <me@lmorchard.com>
2014-10-20 11:53:48 -0400

    Use gulp to concat and uglify JS; include jquery and emphasis in main.js

--- design/js/main.js
+++ design/js/main.js
@@ -1,6 +1,20 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1519392-1']);
+_gaq.push(['_setDomainName', 'blog.lmorchard.com']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+  '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
 $('*[data-content-href]').each(function () {
   var el = $(this);
   $.get(el.attr('data-content-href'), function (data) {
     el.html(data);
   });
 });
+
+}());
