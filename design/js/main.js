fd397025ecff6c202bc89cafc4d76ab9f77fa6d8
Les Orchard <me@lmorchard.com>
2015-07-17 17:53:27 -0400

    Add a quick table of contents enhancement

--- design/js/main.js
+++ design/js/main.js
@@ -1,3 +1,31 @@
 (function () {
 
+  var tocRoot = document.querySelector('article.post nav.table-of-contents');
+  if (tocRoot) {
+
+    var h2s = tocRoot.parentNode.querySelectorAll('h2');
+    if (h2s.length) {
+
+      var title = document.createElement('p');
+      title.classList.add('title');
+      title.innerHTML = 'Contents';
+      tocRoot.appendChild(title);
+
+      var list = document.createElement('ol');
+      tocRoot.appendChild(list);
+
+      for (var idx = 0; idx < h2s.length; idx++) {
+        var h2 = h2s[idx];
+        var item = document.createElement('li');
+        var link = document.createElement('a');
+        link.href = '#' + h2.id;
+        link.innerHTML = h2.innerHTML;
+        item.appendChild(link);
+        list.appendChild(item);
+      }
+
+    }
+
+  }
+
 }());
