28d315adf87cea55d52cd2ae9da09dfa1720b548
Les Orchard <me@lmorchard.com>
2015-08-07 15:32:25 -0400

    Try switching from Google Analytics to Piwik

--- design/js/main.js
+++ design/js/main.js
@@ -1,11 +1,13 @@
-// Google Analytics. Yeah, I know, I'm not super happy about it.
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-33630498-1', 'auto');
-ga('send', 'pageview');
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//analytics.lmorchard.com/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 1]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
 
 (function () {
 
