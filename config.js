4c029ab9a6d3f5869cee02b7265e0ad41908a8c4
Les Orchard <me@lmorchard.com>
2015-10-22 11:42:29 -0400

    Try making Travis CI build my blog

--- config.js
+++ config.js
@@ -10,22 +10,20 @@ exports.site = {
     "short_name": "lmorchard",
     "name": "l.m.orchard",
     "email": "l.m.orchard@pobox.com"
-  },
-  "aws": {
-    "key": "YOUR KEY HERE",
-    "secret": "YOUR SECRET HERE",
-    "bucket": "blog.lmorchard.com",
-    "region": "us-east-1"
   }
-};
-
-exports.aws = {
-  key: "",
-  secret: "",
-  bucket: "blog.lmorchard.com",
-  region: "us-east-1"
 };
 
 exports.fulltextTagFeeds = [
   'mozilla'
 ];
+
+try {
+  exports.aws = require('./aws');
+} catch(e) {
+  exports.aws = {
+    key: process.env.AWS_ACCESS_KEY_ID,
+    secret: process.env.AWS_SECRET_ACCESS_KEY,
+    bucket: "blog.lmorchard.com",
+    region: "us-east-1"
+  };
+}
