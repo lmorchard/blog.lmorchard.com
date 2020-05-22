13a75f91992683eb8a665b026dcf0911459872ee
Les Orchard <me@lmorchard.com>
2020-05-22 00:44:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const util = require("util");
+const path = require("path");
+const globby = require("globby");
+const mkdirp = require("mkdirp");
+const copy = require("recursive-copy");
+const frontmatter = require("front-matter");
+const commonmark = require("commonmark");
+const moment = require("moment");
+
+const fs = ["readFile", "writeFile"].reduce(
+  (acc, name) => ({
+    ...acc,
+    [name]: util.promisify(require("fs")[name]),
+  }),
+  {}
+);
+
+const config = {
+  POSTS_PATH: "../blog.lmorchard.com/posts",
+  BUILD_PATH: "./build",
+  COMMONMARK_PARSER_OPTIONS: {},
+  COMMONMARK_RENDERER_OPTIONS: {},
+};
+
+async function main() {
+  const files = globby.stream(`${config.POSTS_PATH}/**/*.{md,markdown}`);
+  for await (const file of files) {
+    const content = await parseEntry(file);
+    const entryPath = path.join(config.BUILD_PATH, content.attributes.path);
+    await mkdirp(entryPath);
+    await writeFiles(entryPath, {
+      "index.md": content.body,
+      "index.html": content.html,
+      "index.json": JSON.stringify(content.attributes, null, "  "),
+    });
+    if (content.attributes.isDir) {
+      await copy(content.attributes.parentPath, entryPath, {
+        overwrite: true,
+      });
+    }
+  }
+}
+
+const entryNameRE = new RegExp("(\\d{4})-(\\d{2})-(\\d{2})-(.*)");
+const cmReader = new commonmark.Parser(config.COMMONMARK_PARSER_OPTIONS);
+const cmWriter = new commonmark.HtmlRenderer(
+  config.COMMONMARK_RENDERER_OPTIONS
+);
+
+async function parseEntry(file) {
+  const data = await fs.readFile(file, "utf8");
+  const content = frontmatter(data);
+  content.html = cmWriter.render(cmReader.parse(content.body));
+
+  const isDir = path.basename(file).startsWith("index.");
+  const entryName = isDir
+    ? path.basename(path.dirname(file))
+    : path.basename(file).split(".")[0];
+  const [, yy, mm, dd, slug] = entryNameRE.exec(entryName);
+
+  content.attributes = {
+    isDir,
+    slug,
+    entryName,
+    parentPath: path.dirname(file),
+    date: new Date(`${yy}-${mm}-${dd}T12:00:00Z`),
+    ...content.attributes,
+  };
+
+  const date = moment(content.attributes.date);
+  content.attributes.path = `${date.format("YYYY/MM/DD")}/${slug}`;
+
+  return content;
+}
+
+const writeFiles = (basePath, filesToWrite) =>
+  Promise.all(
+    Object.entries(filesToWrite).map(([fn, data]) =>
+      fs.writeFile(path.join(basePath, fn), data)
+    )
+  );
+
+main().catch((err) => console.error(err));
