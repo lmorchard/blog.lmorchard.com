7e5bcc596da6e819fecdf0597c903859d1d7df39
Les Orchard <me@lmorchard.com>
2020-05-23 15:25:04 -0700

    Shuffling tags around

--- templates/layoutPage.js
+++ templates/layoutPage.js
@@ -24,6 +24,11 @@ module.exports = (
   <!DOCTYPE html>
   <html lang="en-us">
     <head>
+      <title>${page.title} - ${site.title}</title>
+      <meta property="og:type" content="article" />
+      <meta property="og:site_name" content="${site.title}" />
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <meta name="author" content="${site.author.full_name}" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
@@ -38,10 +43,6 @@ module.exports = (
           />
         `
       )}
-
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta name="author" content="${site.author.full_name}" />
-
       ${page.tag
         ? html`
             <link
@@ -59,12 +60,6 @@ module.exports = (
               type="application/rss+xml"
             />
           `}
-
-      <meta property="og:type" content="article" />
-      <meta property="og:site_name" content="${site.title}" />
-
-      <title>${page.title} - ${site.title}</title>
-
       ${head}
     </head>
     <body>
