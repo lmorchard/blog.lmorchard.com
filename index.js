c4503859bac55a418fdedb3189f376c1b93c1c1d
Les Orchard <me@lmorchard.com>
2020-05-23 14:36:22 -0700

    Add RSS feeds; extract summaries; refactor more things into submodules

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const path = require("path");
 const util = require('util');
-const copy = require("recursive-copy");
 const mkdirp = require("mkdirp");
 const rimraf = util.promisify(require("rimraf"));
 
 const config = require("./config");
-const { writeFiles } = require("./lib/files");
-const { loadAllPosts, buildAllPosts, buildIndex } = require("./lib/posts");
-const { indexBy } = require("./lib/utils");
+const { copyAssets } = require("./lib/assets");
+const { loadAllPosts, buildAllPosts } = require("./lib/posts");
+const { buildAllIndexes } = require("./lib/indexes");
 
 async function main() {
   await rimraf(config.buildPath);
@@ -16,73 +14,6 @@ async function main() {
   const posts = await loadAllPosts();
   await buildAllPosts(posts);
   await buildAllIndexes(posts);
-}
-
-async function copyAssets() {
-  for (const assetPath of config.assetPaths) {
-    await copy(assetPath, config.buildPath, {
-      overwrite: true,
-    });
-  }
-}
-
-async function buildAllIndexes(postsFull) {
-  const root = config.buildPath;
-  const posts = postsFull.map((post) => post.attributes);
-
-  await writeFiles(root, {
-    "index.html": require(`./templates/indexRecent`)({
-      site: config.site,
-      page: {
-        title: "Home",
-      },
-      posts: posts.slice(0, 20),
-    }),
-    "all.html": require(`./templates/indexAll`)({
-      site: config.site,
-      page: {
-        title: "All Posts",
-      },
-      posts,
-    }),
-    "archives.html": require(`./templates/archives`)({
-      site: config.site,
-      page: {
-        title: "Archives",
-      },
-      posts,
-    }),
-  });
-
-  const postsByYear = indexBy(posts, ({ year }) => year);
-  for (const [year, posts] of Object.entries(postsByYear)) {
-    await buildIndex({
-      basePath: path.join(root, year),
-      title: year,
-      template: "indexYear",
-      posts,
-    });
-  }
-
-  const postsByMonth = indexBy(posts, ({ year, month }) => `${year}/${month}`);
-  for (const [month, posts] of Object.entries(postsByMonth)) {
-    await buildIndex({
-      basePath: path.join(root, month),
-      title: month,
-      template: "indexMonth",
-      posts,
-    });
-  }
-
-  const postsByTag = indexBy(posts, ({ tags = [] }) => tags);
-  for (const [tag, posts] of Object.entries(postsByTag)) {
-    await buildIndex({
-      basePath: path.join(root, "tag", tag),
-      title: tag,
-      template: "indexTag",
-      posts,
-    });
-  }
 }
 
 main().catch((err) => console.error(err));
---
+++ templates/rss.js
@@ -0,0 +1,42 @@
+const { html } = require("../lib/html");
+const moment = require("moment");
+
+// TODO: Use an RSS generating module? This template is just copied
+// over from my old blog generator for initial consistency
+
+module.exports = ({
+  site = {},
+  posts = [],
+  tag = "",
+  maxItems = 15,
+  fullText = false,
+}) => html`
+  <?xml version="1.0" encoding="UTF-8"?>
+  <rss version="2.0">
+    <channel>
+      <title>${tag && `Tag: ${tag} - `}${site.title}</title>
+      <description>${site.subtitle}</description>
+      <link>${site.absolute_baseurl}</link>
+      ${posts.slice(0, maxItems).map(
+        (post) => html`
+      <item>
+          <title>${post.title || "untitled"}</title>
+          ${
+            !fullText &&
+            post.summary &&
+            html` <description>${post.summary}</description> `
+          }
+          ${
+            fullText &&
+            post.content &&
+            html` <description>${post.content}</description> `
+          }
+          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+          <link>${site.absolute_baseurl}/${post.path}/</link>
+          <guid isPermaLink="true">${site.absolute_baseurl}/${post.path}/</guid>
+        </item>
+      `
+      )}
+    </channel>
+  </rss>
+`;
