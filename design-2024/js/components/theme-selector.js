da4af6e4ecda460b33ac1f7d7fc18fb1b5bf6a42
Les Orchard <lorchard@mozilla.com>
2024-07-10 17:27:07 -0700

    putzing around with theme selector

--- design-2024/js/components/theme-selector.js
+++ design-2024/js/components/theme-selector.js
@@ -1,23 +1,19 @@
 // adapted from https://stackoverflow.com/questions/56300132/how-to-override-css-prefers-color-scheme-setting/75124760#75124760
 class ThemeSelector extends HTMLElement {
   connectedCallback() {
-    for (const el of this.querySelectorAll(".icon")) {
-      el.style.display = "none";
-    }
-
-    const button = this.querySelector("button");
-    if (button) {
-      button.addEventListener("click", () => this.toggleColorScheme());
-    }
-
     const scheme = this.getPreferredColorScheme();
     this.applyPreferredColorScheme(scheme);
+
+    const checkbox = this.querySelector("input[type=checkbox]");
+    if (checkbox) {
+      checkbox.checked = scheme === "light";
+      checkbox.addEventListener("change", (ev) =>
+        this.changeColorScheme(ev.target.checked ? "light" : "dark")
+      );
+    }
   }
 
-  toggleColorScheme() {
-    let newScheme = "light";
-    let scheme = this.getPreferredColorScheme();
-    if (scheme === "light") newScheme = "dark";
+  changeColorScheme(newScheme) {
     this.applyPreferredColorScheme(newScheme);
     this.savePreferredColorScheme(newScheme);
   }
@@ -87,16 +83,8 @@ class ThemeSelector extends HTMLElement {
         // ignore "Not allowed to access cross-origin stylesheet" errors
         // from external stylesheets (e.g. Google Fonts)
         if (e.name === "SecurityError") continue;
-        console.warn("theme selector problem:", e);
+        console.warn("theme selector problem:", e, e.name);
       }
-    }
-
-    if (scheme === "dark") {
-      this.querySelector(".icon.light").style.display = "inline";
-      this.querySelector(".icon.dark").style.display = "none";
-    } else {
-      this.querySelector(".icon.dark").style.display = "inline";
-      this.querySelector(".icon.light").style.display = "none";
     }
   }
 }
